78cfc68d4fb51aec0241b0e8a094f63f85a095ac
Vovandir <106840463+Vovandir@users.noreply.github.com>
2022-12-19 22:04:24 +0200

    changes

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
-// import s from './FeedbackOptions.module.css';
 
 function FeedbackOptions({ options, onLeaveFeedback }) {
   return options.map(option => (
-    <button type="button" key={option} onClick={() => onLeaveFeedback(option)}>
+    <button
+      type="button"
+      key={option}
+      onClick={() => onLeaveFeedback(option)}
+    >
       {option}
     </button>
   ));
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,10 +1,9 @@
 import PropTypes from 'prop-types';
-// import s from './Section.module.css';
 
 function Section({ title, children }) {
   return (
     <section>
-      <h2 >{title}</h2>
+      <h2>{title}</h2>
       {children}
     </section>
   );
