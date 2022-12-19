9ad5191e1ad8b3e21d0163fb6d1ed84cfb7128ba
Vovandir <106840463+Vovandir@users.noreply.github.com>
2022-12-19 11:24:45 +0200

    cc

---
+++ src/components/feedback.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Notification from './Notification/notification';
+import Statistics from './Statistics/statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+
+class Feedback extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addFeedback = option => {
+    this.setState(state => ({
+      [option]: state[option] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return ((100 * this.state.good) / this.countTotalFeedback()).toFixed();
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.addFeedback}
+          />
+        </Section>
+
+        <h2>Statistics</h2>
+
+        {this.countTotalFeedback() === 0 ? (
+          <Notification message="No feedback given" />
+        ) : (
+          <Statistics
+            good={good}
+            bad={bad}
+            neutral={neutral}
+            total={this.countTotalFeedback()}
+            positive={this.countPositiveFeedbackPercentage()}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default Feedback;
